963c9ac23844eec73d72af3460f6dd9d7e253e40
806gw <shjea4364@naver.com>
2025-07-24 23:00:11 +0900

    initial commit

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
+import { resolve } from "path";
+
+export default defineConfig({
+  plugins: [
+    react(),
+    vanillaExtractPlugin({
+      identifiers: ({ hash }) => `weesh_${hash}`,
+    }),
+  ],
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "src"),
+    },
+  },
+});
